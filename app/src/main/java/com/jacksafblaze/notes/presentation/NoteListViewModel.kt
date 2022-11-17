cae94cb3c41cda3496c97b64d5533320429997a9
JackSaf <jacksafblaze@gmail.com>
2022-11-17 05:04:18 +0300

    create view model for note list fragment

---
+++ app/src/main/java/com/jacksafblaze/notes/presentation/NoteListViewModel.kt
@@ -0,0 +1,11 @@
+package com.jacksafblaze.notes.presentation
+
+import androidx.lifecycle.ViewModel
+import com.jacksafblaze.notes.domain.model.Note
+
+class NoteListViewModel: ViewModel() {
+
+    fun deleteNote(note: Note){
+
+    }
+}
