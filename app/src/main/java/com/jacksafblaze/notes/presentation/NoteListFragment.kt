5e2cef863e2dfa1fadd8e73e7c294c479d80b546
JackSaf <jacksafblaze@gmail.com>
2022-11-17 05:03:24 +0300

    create note list fragment

---
+++ app/src/main/java/com/jacksafblaze/notes/presentation/NoteListFragment.kt
@@ -0,0 +1,47 @@
+package com.jacksafblaze.notes.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.jacksafblaze.notes.R
+
+
+class NoteListFragment : Fragment() {
+    private lateinit var adapter: NoteAdapter
+    private val viewModel: NoteListViewModel by viewModels()
+    private val itemTouchHelper = object: ItemTouchHelper.SimpleCallback(0, (ItemTouchHelper.LEFT.or(
+        ItemTouchHelper.RIGHT))){
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean = true
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val pos = viewHolder.layoutPosition
+            val item = adapter.differ.currentList[pos]
+            viewModel.deleteNote(item)
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
