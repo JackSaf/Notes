d0e164f83b59009968150a74fa3b77bd4e9f7504
JackSaf <jacksafblaze@gmail.com>
2022-11-17 23:54:24 +0300

    add use case for fetching

---
+++ app/src/main/java/com/jacksafblaze/notes/domain/usecases/FetchNotesUseCase.kt
@@ -0,0 +1,9 @@
+package com.jacksafblaze.notes.domain.usecases
+
+import com.jacksafblaze.notes.domain.repository.NoteRepository
+
+class FetchNotesUseCase(repository: NoteRepository) {
+    suspend fun execute(){
+
+    }
+}
