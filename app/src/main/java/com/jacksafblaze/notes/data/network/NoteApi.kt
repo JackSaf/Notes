ee411bd835732f05883e0bfc24f6f1bacbe9603b
JackSaf <jacksafblaze@gmail.com>
2022-11-17 23:56:42 +0300

    add dispatcher io

--- app/src/main/java/com/jacksafblaze/notes/data/network/NoteApi.kt
+++ app/src/main/java/com/jacksafblaze/notes/data/network/NoteApi.kt
@@ -1,10 +1,12 @@
 package com.jacksafblaze.notes.data.network
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 class NoteApi {
-    suspend fun fetchNotes(): List<NoteNetworkEntity>{
+    suspend fun fetchNotes(): List<NoteNetworkEntity> = withContext(Dispatchers.IO){
         delay(5000)
-        return listOf()
+        listOf()
     }
 }
