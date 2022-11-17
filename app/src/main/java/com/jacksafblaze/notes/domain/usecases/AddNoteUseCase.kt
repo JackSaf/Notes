d71a6f9ef4060a2ceb9039004b8a8149ee430735
JackSaf <jacksafblaze@gmail.com>
2022-11-17 23:55:08 +0300

    renamed use case

--- app/src/main/java/com/jacksafblaze/notes/domain/usecases/AddNoteUseCase.kt
+++ app/src/main/java/com/jacksafblaze/notes/domain/usecases/AddNoteUseCase.kt
@@ -3,7 +3,7 @@ package com.jacksafblaze.notes.domain.usecases
 import com.jacksafblaze.notes.domain.model.Note
 import com.jacksafblaze.notes.domain.repository.NoteRepository
 
-class AddDefaultNoteUseCase(private val repository: NoteRepository) {
+class AddNoteUseCase(private val repository: NoteRepository) {
     suspend fun execute(note: Note){
         repository.addNote(note)
     }
