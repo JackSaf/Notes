7e9f6f71885210df6db57c33ffba682e5382c55a
JackSaf <jacksafblaze@gmail.com>
2022-11-18 04:35:08 +0300

    add autoincrement for primary key

--- app/src/main/java/com/jacksafblaze/notes/data/database/NoteDbDto.kt
+++ app/src/main/java/com/jacksafblaze/notes/data/database/NoteDbDto.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "notes")
 data class NoteDbDto(
-    @PrimaryKey val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val title: String,
     val description: String,
     @ColumnInfo(name = "last_changes_date")val lastChangesDate: String,
