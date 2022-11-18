ea7986ca2bd7088a6f162046aacf9fc886b54ddf
JackSaf <jacksafblaze@gmail.com>
2022-11-18 21:48:44 +0300

    create alarm manager to fire at 12 o'clock

--- app/src/main/java/com/jacksafblaze/notes/MainActivity.kt
+++ app/src/main/java/com/jacksafblaze/notes/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.jacksafblaze.notes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jacksafblaze.notes.service.UpdateNotesService
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        val pendingIntent = PendingIntent.getService(
+            applicationContext,
+            0,
+            Intent(applicationContext, UpdateNotesService::class.java),
+            PendingIntent.FLAG_UPDATE_CURRENT.or(PendingIntent.FLAG_IMMUTABLE)
+        )
+        val alarmManager = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP, calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY, pendingIntent
+        )
     }
 }
